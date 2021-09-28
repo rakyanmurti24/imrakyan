662c3bc336bd0112279d5d2d4e9c00c0d3c0e368
Rakyanmurti <widodorakyan@gmail.com>
2021-09-28 20:05:04 +0700

    projek git

---
+++ Contoh1.php
@@ -0,0 +1,14 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Rakyan Tri Mustika Murtirani
+            Saya tinggal dirumah orangtua saya
+            olahraga yang saya sukai adalah membaca
+            terimakasih";
+            
+    }
+}
---
+++ view-latihan.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    Halo sahabat.. yuk kita belajar web progamming..!!!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini hasi dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+
+</body>
+
+</html>
