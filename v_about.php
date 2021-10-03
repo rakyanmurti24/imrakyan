1914d6dc066f3a929841e4dd06792c772d36ada2
rakyanmurti24 <91543754+rakyanmurti24@users.noreply.github.com>
2021-10-03 13:50:17 +0700

    tugas pertemuan 3

---
+++ v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4> Nama </h4>
+    <ul type="disc">
+        <li>Nama Depan : Rakyan</li>
+        <li>Nama Belakang : Murti</li>
+</ul>
+<br>
+<h4>Alamat</h4>
+<ul type="none">
+    <li> Jalan Ciapus Raya No.212 Badak putih</li>
+</ul>
+
+<h4>Tempat Lahir</h4>
+<ul type="none">
+    <li>Bogor</li>
+</ul>
+
+<h4> Olahraga Favorit</h4>
+<ul type="square">
+    <li>Berenang</li>
+    <li>swimming</li>
+</ul>
+</section>
